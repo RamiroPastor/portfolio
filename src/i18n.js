b6e96e3d9374753d13fb6d698afab614faa4d0f3
Ramiro <ramir659@gmail.com>
2021-02-27 15:58:50 +0100

    fixed language cookie bug

--- src/i18n.js
+++ src/i18n.js
@@ -22,7 +22,7 @@ i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: langCookie === "" ? "es" : "en",
+        lng: langCookie === "" ? "es" : langCookie,
 
         interpolation: {
             escapeValue: false // react already safes from xss

bc53d89838ad4efb7f4828ddd2d36cc70b15d2ec
Ramiro <rammir659@gmail.com>
2022-09-24 00:22:09 +0200

    now using haskell package svg-icons !!!

--- src/assets/svg/carnet.jsx
+++
@@ -1,7 +0,0 @@
-export const carnetSvg = 
-<svg viewBox="0 0 1 1" preserveAspectRatio="xMinYMin meet">
-    <path strokeLinejoin="round" strokeWidth="4.0e-2" fill="transparent" d="M 3.078000000000003e-2,0.21000000000000002 L 0.96922,0.21000000000000002 L 0.96922,0.79 L 3.078000000000003e-2,0.79 Z" />
-    <path strokeWidth="0.035" strokeLinecap="round" d="M 0.58,0.38 L 0.83,0.38 M 0.58,0.5 L 0.83,0.5 M 0.58,0.62 L 0.83,0.62 " />
-    <circle cx="0.29500002" cy="0.44" r="0.07" stroke="none" />
-    <path d="M 0.17000002,0.62 A 0.125,0.125 0.0 1,1 0.42000002,0.62 A 0.125,2.5e-2 0.0 1,1 0.17000002,0.62 " stroke="none" />
-</svg>
